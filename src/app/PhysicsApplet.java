53f47b794daba9f253c6a4379abf7e0ebed29cfc
Júlio Leitão Jr <julio.leitao.jr@gmail.com>
2014-02-14 09:49:44 -0300

    Update

---
+++ src/app/PhysicsApplet.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class PhysicsApplet extends Applet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void paint(Graphics graphic) {
+		graphic.drawString("Welcome to a Java Applet", 20, 20);
+	}
+}
+
+
